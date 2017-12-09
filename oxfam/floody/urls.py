47dce4dea4def7291fd3cc30aad08a6835c305bc
sinh <vts3497@gmail.com>
2017-12-09 11:52:42 +0700

    model + service

--- oxfam/floody/urls.py
+++ oxfam/floody/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     url(r'^accounts/registered/$', views.register_complete, name='register complete'),
     url(r'^accounts/loggedin/$', views.loggin_view, name='logged in'),
     url(r'^accounts/loggedout/$', views.logout_view, name='logged out'),
+    url(r'^campost/$', views.camimage , name='campost'),
+
 ]
